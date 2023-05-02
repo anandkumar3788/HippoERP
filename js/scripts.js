6c9d916f28d6bd3fe55c0093b8b4212df37ace43
anandkumar3788 <116706160+anandkumar3788@users.noreply.github.com>
2023-05-02 22:56:01 +0530

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#mydata').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
+    } );
+} );
